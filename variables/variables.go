27eb88b68e1bda188c79040d5f224b88d550dc61
Sachin <sachin6624@gmail.com>
2023-09-13 23:13:25 +0530

    complex types and casting

--- variables/variables.go
+++ variables/variables.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
+var globalVariable string = "Nothing" //shot assignment operator dont work outside a function
+
 func main() {
 	//long assignbment operator
 	var smsSendingLimit string = "Sachin"
 	//short assignment operatior
 	username := "samray"
+
+	fmt.Println(globalVariable)
 
 	//Example variables
 	status := true
@@ -22,5 +26,15 @@ func main() {
 	// Initailize multiple variable in single line
 	name, age, id := "Sachin", 22, 23.23
 	fmt.Println(name, age, id)
+
+	//casting of type
+	height := 178.7
+	newCast := int(height) //cutting the fractions
+	additionalCast := float64(newCast)
+	fmt.Println(additionalCast)
+
+	i := 42 // int
+	g := 0.867 + 0.5i
+	fmt.Printf("the compleNum type is %T %v\n", g, i)
 
 }
