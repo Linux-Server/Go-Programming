4eca17799844b5ea2f4e27e4726d7cbab50c446b
Sachin <sachin6624@gmail.com>
2023-10-03 19:21:42 +0530

    task 10 static typing

---
+++ boot-dev/10_strong_type.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// go is a statically typed lang, that means varibales can only have a single type
+	username := "sachin"
+	password := "12345"
+
+	fmt.Println("The basic auth is : ", username+"@"+password)
+}
